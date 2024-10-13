43930261f161e39f9d6381e954c3c23fd931ed9e
Martin Angermair <martin.angermair@oebb.at>
2024-10-13 07:26:43 +0200

    Add user error checking

--- chap_01/friendship.py
+++ chap_01/friendship.py
@@ -1,4 +1,4 @@
-# Initialize the users and their relationship
+# Initialize the users
 users = [
     { "id": 0, "name": "Hero"},
     { "id": 1, "name": "Dunn"},
@@ -12,14 +12,22 @@ users = [
     { "id": 9, "name": "Klein"},
 ]
 
+# Initialize the friendship betwee the Users
 friendship_pairs = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
                     (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
 
+# Initialize user list with empty friendships
 friendships = {user["id"]: [] for user in users}
-for i, j in friendship_pairs:
-    friendships[i].append(j)
-    friendships[j].append(i)
 
+# Fill friendships with friends
+for i, j in friendship_pairs:
+    if i in friendships and j in friendships:
+        friendships[i].append(j)
+        friendships[j].append(i)
+    else:
+        print(f"Warning: User with ID {i} or {j} does not exists.")
+
+# Print out the result
 print(f"Benutzer: {users}")
 print(f"Beziehungen: {friendship_pairs}")
 print(f"Freundschaften: {friendships}")
